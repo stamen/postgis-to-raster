92030926ee2a6e7f91efdd498df940ed7eee0439
Eric Gelinas <evg@stamen.com>
2014-02-14 17:51:34 -0800

    make the nodejs interface

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 'use strict';
 
+module.export = require('./postgis-to-raster.js')(config.postgis);
+
+//
+// This will likely grow into more of a specialized
+// interface for calling from NodeJS
+//
